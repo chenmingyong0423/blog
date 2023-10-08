2419c08d0d0653b08aceda276202012a6aed2139
chenmingyong0423 <474571928@qq.com>
2023-10-08 21:58:25 +0800

    slog 教程

---
+++ tutorial-code/slog/demo5/main.go
@@ -0,0 +1,27 @@
+// Copyright 2023 chenmingyong0423
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	jsonLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(jsonLogger)
+	slog.InfoContext(context.Background(), "JsonHandler", "姓名", "陈明勇") //{"time":"2023-10-08T21:11:22.41760604+08:00","level":"INFO","msg":"JsonHandler","姓名":"陈明勇"}
+}
