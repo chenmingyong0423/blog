262a008b00c07e8d705b505dffd231548ca6de07
chenmingyong <chenmingyong5873@126.com>
2023-11-13 15:42:23 +0800

    query

--- tutorial-code/go-mongox/collection/builder/query.go
+++ tutorial-code/go-mongox/collection/builder/query.go
@@ -15,20 +15,39 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chenmingyong0423/go-mongox/bsonx"
 	"github.com/chenmingyong0423/go-mongox/builder/query"
 	"github.com/chenmingyong0423/go-mongox/types"
 )
 
 func main() {
-	query.BsonBuilder().Add(types.KV("姓名", "陈明勇")).Build() // bson.D{{Key:"姓名", Value:"陈明勇"}}
+	// bson.D{bson.E{Key:"姓名", Value:"陈明勇"}}
+	d := query.BsonBuilder().Add(types.KV("姓名", "陈明勇")).Build()
+	fmt.Printf("%#v\n", d)
 
-	query.BsonBuilder().And(bsonx.M("姓名", "陈明勇"), query.BsonBuilder().Gte("age", 25).Build()).Build() // bson.D{{Key:"$and", Value:[]any{bson.M{"姓名": "陈明勇"}, bson.D{{Key:"age", Value:bson.D{{Key:"$gte", Value:25}}}}}}}
+	// bson.D{bson.E{Key:"age", Value:bson.D{{Key:"$gt", Value:18}, bson.E{Key:"$lt", Value:25}}}}
+	d = query.BsonBuilder().Gt("age", 18).Lt("age", 25).Build()
+	fmt.Printf("%#v\n", d)
 
-	query.BsonBuilder().Or(bsonx.M("姓名", "陈明勇"), bsonx.M("姓名", "南")).Build() // bson.D{{Key:"$or", Value:[]any{bson.M{"姓名": "陈明勇"}, bson.M{"姓名": "南"}}}}
+	//  bson.D{bson.E{Key:"age", Value:bson.D{{Key:"$in", Value:[]int{18, 19, 20}}}}}
+	d = query.BsonBuilder().InInt("age", 18, 19, 20).Build()
+	fmt.Printf("%#v\n", d)
 
-	query.BsonBuilder().InString("tags", "mongodb", "golang").Build() // bson.D{{Key:"tags", Value:bson.M{"$in": []string{"mongodb", "golang"}}}}
+	// bson.d{bson.E{Key: "$and", Value: []any{bson.D{{Key: "x", Value: bson.D{{Key: "$ne", Value: 0}}}}, bson.D{{Key: "y", Value: bson.D{{Key: "$gt", Value: 0}}}}}}
+	d = query.BsonBuilder().And(
+		query.BsonBuilder().Ne("x", 0).Build(),
+		query.BsonBuilder().Gt("y", 0).Build(),
+	).Build()
+	fmt.Printf("%#v\n", d)
 
-	query.BsonBuilder().And(query.BsonBuilder().Gt("age", 15).Build(), query.BsonBuilder().Lt("age", 25).Build()).Build() // bson.d{{Key:"$and", Value:[]any{bson.D{{Key:"age", Value:bson.M{"$gt": 15}}}, bson.D{{Key:"age", Value:bson.M{"$lt": 25}}}}}}
+	// bson.D{bson.E{Key:"qty", Value:bson.D{{Key:"$exists", Value:true}, bson.E{Key:"$nin", Value:[]int{5, 15}}}}}
+	d = query.BsonBuilder().Exists("qty", true).NinInt("qty", 5, 15).Build()
+	fmt.Printf("%#v\n", d)
 
+	// elemMatch
+	// bson.D{bson.E{Key: "result", Value: bson.D{bson.E{Key: "$elemMatch", Value: bson.D{bson.E{Key: "$gte", Value: 80}, bson.E{Key: "$lt", Value: 85}}}}}}
+	d = query.BsonBuilder().ElemMatch("result", bsonx.D(types.KV("$gte", 80), types.KV("$lt", 85))).Build()
+	fmt.Printf("%#v\n", d)
 }
