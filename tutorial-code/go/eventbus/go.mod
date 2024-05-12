1cb42b91941744ead0a2f0f94b9483bd21a96ae5
Mingyong Chen <chenmingyong5873@126.com>
2024-05-13 03:27:26 +0800

    新增 go eventbus 文章的示例代码

---
+++ tutorial-code/go/eventbus/go.mod
@@ -0,0 +1,5 @@
+module github.com/chenmingyong0423/blog/tutorial-code/go/eventbus
+
+go 1.22.0
+
+require github.com/chenmingyong0423/go-eventbus v0.0.0-20240512192141-bc0bdc51c7c2 // indirect
---
+++ tutorial-code/go/eventbus/main.go
@@ -0,0 +1,47 @@
+// Copyright 2024 chenmingyong0423
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"github.com/chenmingyong0423/go-eventbus"
+	"time"
+)
+
+func main() {
+	eventBus := eventbus.NewEventBus()
+
+	// 订阅 post 主题事件
+	subscribe := eventBus.Subscribe("post")
+
+	go func() {
+		for event := range subscribe {
+			fmt.Println(event.Payload)
+
+		}
+	}()
+
+	eventBus.Publish("post", eventbus.Event{Payload: map[string]any{
+		"postId": 1,
+		"title":  "Go 事件驱动编程：实现一个简单的事件总线",
+		"author": "陈明勇",
+	}})
+	// 不存在订阅者的 topic
+	eventBus.Publish("pay", eventbus.Event{Payload: "pay"})
+
+	time.Sleep(time.Second * 2)
+	// 取消订阅 post 主题事件
+	eventBus.Unsubscribe("post", subscribe)
+}
